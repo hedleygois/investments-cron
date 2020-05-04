478442443ccf4bd69c73c26c16adf412970e0658
Hedley Luna <hluna@stackstate.com>
2020-05-05 01:04:47 +0200

    fixing query

--- src/util/Queries.ts
+++ src/util/Queries.ts
@@ -16,8 +16,8 @@ export const QUERY_BY_TICKER = `
 `;
 
 export const SAVE_STOCK = `
-  mutation SaveStock($symbol: String!, $open: float8, $price: float8!, $high: float8, $low: float8, $volume: float8, $latest: timestamptz, $previous: float8, $changeAbs: float8, $changeP: float8) {
-    insert_Stock(objects: {symbol: $symbol, open: $open, price: $price, high: $high, low: $low, volume: $volume, latest: $latest, previous: $previous, changeAbs: $changeAbs, changeP: $changeP}) {
+  mutation SaveStock($symbol: String!, $open: float8, $price: float8!, $high: float8, $low: float8, $volume: float8, $latest: timestamptz, $previous: float8, $changeAbs: float8, $changeP: float8, active: Boolean) {
+    insert_Stock(objects: {symbol: $symbol, open: $open, price: $price, high: $high, low: $low, volume: $volume, latest: $latest, previous: $previous, changeAbs: $changeAbs, changeP: $changeP, active: $active}) {
       affected_rows,
       returning {
         id
